c6453727822d7cd40e1d385a30e0a4533e2ff1b6
INASAKUPC\inasa <inasakun@gmail.com>
2025-09-23 07:08:30 +0000

    add:アイコンを追加

--- app/frontend/src/app/app.routes.ts
+++ app/frontend/src/app/app.routes.ts
@@ -11,7 +11,7 @@ export const routes: Routes = [
   {
     path: '',
     component: Home,
-    title: 'Home',
+    title: 'Budget',
   },
   {
     path: 'calender',
--- app/frontend/src/app/app.ts
+++ app/frontend/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterModule } from '@angular/router';
+import { Header } from './components/header/header';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterModule],
+  imports: [RouterOutlet, RouterModule, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
