be88aedfb161c4e683242018a6e820c366a016fa
hodaka.suhama <hodaka.suhama@sola-air.com>
2025-10-09 04:00:18 +0000

    add:ヘッダーの内容を動的に変更するロジックを追加

--- app/frontend/src/app/components/header/header.ts
+++ app/frontend/src/app/components/header/header.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
 export class Header {
+  //現在のURLに応じて、ヘッダーのログインボタンと登録ボタンの表示/非表示を制御する
+  //locationの移動の際にヘッダーは初期化されないため、違う方法で現在のURLを管理する必要がある
+  currentRoute: string;
+  authButtonsVisibleRoutes: string[] = ['/login', '/register', '/forgot-password', '/reset-password', '/'];
+  authButtonsVisible: boolean;
 
+  constructor(private router: Router) {
+    this.currentRoute = this.router.url;
+    this.authButtonsVisible = this.authButtonsVisibleRoutes.includes(this.currentRoute);
+    console.log('Current Route:', this.currentRoute);
+  }
 }
