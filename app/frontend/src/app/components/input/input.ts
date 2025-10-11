b3c64cbbfed72eb20761eae7717ee4712ea58ef8
INASAKUPC\inasa <inasakun@gmail.com>
2025-10-11 07:33:02 +0000

    add:入力画面にフォームとボタンを設置

--- app/frontend/src/app/components/input/input.ts
+++ app/frontend/src/app/components/input/input.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-input',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './input.html',
   styleUrl: './input.css'
 })
 export class Input {
+  inputControl = new FormControl('');
+
+  get value() {
+    return this.inputControl.value;
+  }
+
+  // 金額登録
+  register() {
+    console.log(this.value);
+  }
 
 }
