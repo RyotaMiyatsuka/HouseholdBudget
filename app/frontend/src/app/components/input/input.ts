01d453aeaf55c8883ae3a06806a4c97883525c0f
INASAKUPC\inasa <inasakun@gmail.com>
2025-10-16 10:26:38 +0000

    add:ジャンル選択時のエフェクトを追加

--- app/frontend/src/app/components/input/input.ts
+++ app/frontend/src/app/components/input/input.ts
@@ -10,6 +10,12 @@ import { FormControl, ReactiveFormsModule } from '@angular/forms';
 export class Input {
   inputControl = new FormControl('');
 
+  selectedGenreIndex = 0;
+
+  selectGenre(index: number) {
+    this.selectedGenreIndex = index;
+  }
+
   genres = ['分類', '分類', '分類', '分類', '分類', '分類', '分類', '分類', '分類', '分類', '+'];
 
   get value() {
