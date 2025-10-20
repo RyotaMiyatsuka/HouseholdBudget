df65a04c1c64fa7ae22d727af734cb68526539b6
hodaka.suhama <hodaka.suhama@sola-air.com>
2025-10-20 08:39:59 +0000

    update:scssで書いていた部分をTailwindに修正

--- app/frontend/src/app/components/settings/settings.ts
+++ app/frontend/src/app/components/settings/settings.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { Auth } from '../../services/auth';
 
 @Component({
   selector: 'app-settings',
@@ -7,5 +9,14 @@ import { Component } from '@angular/core';
   styleUrl: './settings.scss'
 })
 export class Settings {
+  private router = inject(Router);
+  private authService = inject(Auth); // AuthServiceを注入
 
+  logout() {
+    // ログアウト処理をここに実装
+    // 例: 認証サービスのlogoutメソッドを呼び出すなど
+    console.log('ログアウトしました');
+    this.authService.logout(); // Authサービスのlogoutメソッドを呼び出す
+    this.router.navigate(['/']); // ログインページへリダイレクト
+  }
 }
